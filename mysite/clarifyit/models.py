de610606338e9fcf0159f2637d999c58cb61c00b
karthikeyan <karthikeyan@testpress.in>
2022-12-24 18:41:55 +0530

    Create Question,Answer and Comment models

--- mysite/clarifyit/models.py
+++ mysite/clarifyit/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Question(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="question"
+    )
+    description = models.TextField()
+    created = models.DateTimeField(default=timezone.now)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ("-created",)
+
+    def __str__(self):
+        return self.title
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(
+        Question, on_delete=models.CASCADE, related_name="answers"
+    )
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ("created",)
+
+    def __str__(self):
+        return f"Answered by {self.name} on {self.question}"
+
+
+class Comment(models.Model):
+    answer = models.ForeignKey(
+        Answer, on_delete=models.CASCADE, related_name="comments"
+    )
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ("created",)
+
+    def __str__(self):
+        return f"commented by {self.name} on {self.answer}"
