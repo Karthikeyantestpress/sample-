6dc3d22f90118a9b0ec77a7282e759682af23c73
karthikeyan <karthikeyan@testpress.in>
2022-12-24 18:39:36 +0530

    Create clarifyit app

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "clarifyit.apps.ClarifyitConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
