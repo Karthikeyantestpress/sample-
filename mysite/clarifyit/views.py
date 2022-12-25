9c9ae08930e5cd6d9b5f32f692073a69f5d6cc87
karthikeyan <karthikeyan@testpress.in>
2022-12-25 16:37:23 +0530

    home added

--- mysite/clarifyit/views.py
+++ mysite/clarifyit/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 def default_page(request):
     return render(request, "base.html")
+
+
+@login_required
+def home(request):
+    return render(request, "clarifyit/home.html")
