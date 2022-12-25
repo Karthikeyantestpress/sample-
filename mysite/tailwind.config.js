1ceced0d46e8dae65caad6c90f5ce0fe8e5e2146
karthikeyan <karthikeyan@testpress.in>
2022-12-25 16:00:55 +0530

    login

---
+++ mysite/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["clarifyit/templates/**/*.{html,js}",
+            "clarifyit/templates/registration/**/*.{html,js}",
+            "clarifyit/static/css/**/*.{html,js}" ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
